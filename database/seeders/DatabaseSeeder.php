0fa21b28260cae877ba8e4c7fb1fec736cb825e2
3dgarJo3star <edgar.tinoco2110@outlook.com>
2026-02-22 00:09:50 -0600

    seeder just created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{Post, User};
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,16 +10,42 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        
+        // SYSADMIN
+        User::create([
+            'first_name' => 'System',
+            'last_name' => 'Administrator',
+            'birth_date' => '1990-01-01',
+            'email' => 'sysadmin@example.com',
+            'username' => 'sysadmin',
+            'password' => bcrypt('Admin_12345'),
+            'email_verified_at' => now(),
         ]);
+
+        // EDGAR
+        User::create([
+            'first_name' => 'Edgar',
+            'last_name' => 'Tinoco',
+            'birth_date' => '2004-10-21',
+            'email' => 'edgar@example.com',
+            'username' => 'e_tinoco',
+            'password' => bcrypt('Temporal_2110'),
+            'email_verified_at' => now(),
+        ]);
+
+        // ALEJANDRA
+        User::create([
+            'first_name' => 'Alejandra',
+            'last_name' => 'Ovalle',
+            'birth_date' => '2004-11-03',
+            'email' => 'alejandra@example.com',
+            'username' => 'champagneecoastt',
+            'password' => bcrypt('Temporal_2110'),
+            'email_verified_at' => now(),
+        ]);
+
+        Post::factory(10)->create();
     }
 }
